73f7d53729d13aa3c929fe838dda9b90987bd761
Miguel Lorenzo <jsmglorenzo@gmail.com>
2020-08-22 02:55:41 -0400

    updated config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    REDIRECT_URL: "https://miguel-lorenzo.dev"
+    REDIRECT_URI: "https://miguel-lorenzo.dev"
 }

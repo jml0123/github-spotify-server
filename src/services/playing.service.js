17385a6e366713bb0f4c63ea90772cf10794770b
miguel l <m_n_l@miguels-mbp.mynetworksettings.com>
2024-02-02 13:00:54 -0500

    refactoring

--- src/services/playing.service.js
+++ src/services/playing.service.js
@@ -2,56 +2,54 @@
 const {getAuthToken} = require('../apis/spotify');
 
 const getCurrentlyPlaying = async (req, res) => {
-    const {access_token} = await getAuthToken()
-
-    const trackData = {
-        playing: null,
-        artist: null,
-        track: null,
-        album: null,
-        releaseDate: null,
-        url: null,
-        imageUrl: null,
-        type: null,
-    };
     try {
-        const currentlyPlaying = await (
-        await fetch("https://api.spotify.com/v1/me/player/currently-playing?additional_types=episode", {
-          headers: {
-            Authorization: `Bearer ${access_token}`,
-          },
-        })
-      )
-      const data = await currentlyPlaying.json()
-      if (!data || !data.item) {
-        trackData.playing = false;
-        res.status(200).json(trackData);
-      }
-      else if(data.currently_playing_type === "track") {
-        trackData.playing = true;
-        trackData.artist = data.item.artists.map(a => a.name).join(", ");
-        trackData.track = data.item.name;
-        trackData.album = data.item.album.name;
-        trackData.releaseDate = data.item.album.release_date;
-        trackData.imageUrl = data.item.album.images[0].url;
-        trackData.url = data.item.external_urls.spotify;
-      }
-      else if (data.currently_playing_type === "episode") {
-        trackData.playing = true;
-        trackData.artist = data.item.show.name;
-        trackData.track = data.item.name;
-        trackData.url = data.item.show.external_urls.spotify;
-        trackData.releaseDate = data.item.release_date;
-        trackData.imageUrl = data.item.show.images[0].url;
-      }
-      trackData.type = data.currently_playing_type;
+        const { access_token } = await getAuthToken();
 
-      res.status(200)
-      .json(trackData)
-    } 
-      catch(err){ 
-        console.log(err)
-      }    
-}
+        const trackData = {
+            playing: null,
+            artist: null,
+            track: null,
+            album: null,
+            releaseDate: null,
+            url: null,
+            imageUrl: null,
+            type: null,
+        };
+
+        const currentlyPlaying = await fetch("https://api.spotify.com/v1/me/player/currently-playing?additional_types=episode", {
+            headers: {
+                Authorization: `Bearer ${access_token}`,
+            },
+        });
+
+        const data = await currentlyPlaying.json();
+
+        if (!data || !data.item) {
+            trackData.playing = false;
+        } else if (data.currently_playing_type === "track") {
+            trackData.playing = true;
+            trackData.artist = data.item.artists.map(a => a.name).join(", ");
+            trackData.track = data.item.name;
+            trackData.album = data.item.album.name;
+            trackData.releaseDate = data.item.album.release_date;
+            trackData.imageUrl = data.item.album.images[0].url;
+            trackData.url = data.item.external_urls.spotify;
+        } else if (data.currently_playing_type === "episode") {
+            trackData.playing = true;
+            trackData.artist = data.item.show.name;
+            trackData.track = data.item.name;
+            trackData.url = data.item.show.external_urls.spotify;
+            trackData.releaseDate = data.item.release_date;
+            trackData.imageUrl = data.item.show.images[0].url;
+        }
+
+        trackData.type = data.currently_playing_type;
+
+        res.status(200).json(trackData);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+};
 
 module.exports = {getCurrentlyPlaying};
